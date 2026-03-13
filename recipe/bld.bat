0d43fc9ccdf3ceed9ad6de19fd159901a37acf54
Kevin Marchais <kevinmarchais@gmail.com>
2026-03-13 11:31:15 +0100

    Add VTK build variant

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ cmake -G "Ninja" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D BUILD_SHARED_LIBS=ON  ^
       -D "CMAKE_INSTALL_PREFIX=%PREFIX%" ^
-      -D USE_VTK=OFF ^
+      -D USE_VTK=%use_vtk% ^
       -D MMG5_PACKAGE:BOOL=OFF ^
       -S .  ^
       -B builddir
