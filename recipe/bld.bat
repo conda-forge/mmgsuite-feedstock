a136484256272279c7697089670fc42a532297d2
Felipe Bordeu <felipebordeu@gmail.com>
2026-09-08 20:56:46 +0930

    second try

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,10 @@
+copy /Y "%RECIPE_DIR%\FindVTK.cmake" "cmake\modules\FindVTK.cmake"
+if errorlevel 1 exit 1
+
 cmake -G "Ninja" ^
       -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_CXX_STANDARD=17 ^
+      -D CMAKE_CXX_STANDARD_REQUIRED=ON ^
       -D BUILD_SHARED_LIBS=ON  ^
       -D "CMAKE_INSTALL_PREFIX=%PREFIX%" ^
       -D USE_VTK=%use_vtk% ^
